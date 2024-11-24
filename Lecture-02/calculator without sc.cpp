4654b6280c8a2f96aee1c3c0d264d2d54509d285
AdrishikharChowdhury <amiadrishikhar@gmail.com>
2024-11-24 19:59:52 +0530

    Initial commit

---
+++ Lecture-02/calculator without sc.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter 1st no. ";
+    cin>>a;
+     cout<<"Enter 2nd no. ";
+    cin>>b;
+    cout<<"Sum: "<<(a+b)<<endl;
+    cout<<"Difference: "<<(a-b)<<endl;
+    cout<<"Multiplication: "<<(a*b)<<endl;
+    cout<<"Quotient: "<<(a/b)<<endl;
+    cout<<"Remainder: "<<(a%b)<<endl;
+}
